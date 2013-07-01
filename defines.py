6e18ede5ad7e9b67565ad5255d6a47f807009e16
peecky <crimsonpi@gmail.com>
2013-07-01 14:19:45 +0900

    add a sample of config file. define some constants

---
+++ defines.py
@@ -0,0 +1,40 @@
+import ConfigParser
+import os
+import cPickle as pickle
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__)) + '/'
+
+config = ConfigParser.SafeConfigParser({
+	'FETCH_POST': '3',
+	'ENQUEUE_POST': '47',
+	'DELETE_OLD_FILES': '4',
+})
+config.read(BASE_PATH + 'config.ini')
+
+SERVER_BASE_ADDRESS = config.get('server', 'SERVER_BASE_ADDRESS')
+USER_AGENT = 'Opera/9.80 (X11; Linux i686) Presto/2.12.388 Version/12.15'
+
+WWW_DATA_PATH = BASE_PATH + config.get('local_path', 'WWW_PATH') + config.get('local_path', 'WWW_DATA_PATH')
+if WWW_DATA_PATH[-1] != '/':
+	WWW_DATA_PATH += '/'
+
+DB_HOST = config.get('database', 'DB_HOST')
+DB_USER_NAME = config.get('database', 'DB_USER_NAME')
+DB_PASSWORD = config.get('database', 'DB_PASSWORD')
+DB_DB_NAME = config.get('database', 'DB_DB_NAME')
+DB_TABLE_PREFIX = config.get('database', 'DB_TABLE_PREFIX')
+
+THUMBNAIL_DATA_PATH = IMAGE_DATA_PATH + 'thumbnail/'
+
+# database
+TABLE_PREFETCHING_QUEUE = DB_TABLE_PREFIX + "prefetchingQueue"
+TABLE_POST = DB_TABLE_PREFIX + "post"
+TABLE_BACKGROUND_JOBS = DB_TABLE_PREFIX + "backgroundJob"
+
+FETCH_POST_DURATION = config.getint('background_job_duration', 'FETCH_POST')
+ENQUEUE_POST_DURATION = config.getint('background_job_duration', 'ENQUEUE_POST')
+DELETE_OLD_FILES_DURATION = config.getint('background_job_duration', 'DELETE_OLD_FILES')
+
+FETCH_POST_MAX_RETRY = 5
+
+PICKLE_PROTOCOL = max(2, pickle.HIGHEST_PROTOCOL)
