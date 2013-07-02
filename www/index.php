5d4e5036cf0f6c26ce01341ca161bc16900d8f49
peecky <crimsonpi@gmail.com>
2013-07-03 04:13:31 +0900

    show the number of unread posts

--- www/index.php
+++ www/index.php
@@ -1,6 +1,17 @@
 <?php
 require_once("common.php");
 require_once("function.php");
+
+if ($session["isNormalUser"]) {
+	$mysql = &getMysqlUtil();
+
+	$result = $mysql->query("select count(*) count_ from " . TABLE_ACTIVE_ITEM . "
+		where userId = :userId
+		and isRead = FALSE", array(
+		":userId" => $session["userId"]
+	));
+	$values["newPostsCount"] = $result[0]["count_"];
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,6 +21,7 @@ require_once("function.php");
 <link rel="stylesheet" href="persona_buttons.css" />
 </head>
 <body>
+<h1><a href="/"><?= htmlspecialchars($values["title"]) ?></a></h1>
 <?php if (!$session["isValidUser"]) { ?>
 <section id="signin">
 	<h2>Sign in</h2>
@@ -20,6 +32,12 @@ require_once("function.php");
 	<h2>Sign out</h2>
 	<?php if ($session["authType"] == AUTH_TYPE_PERSONA) { printPersonaSigninModule($session, TRUE); } ?>
 </section>
+	<?php if ($session["isNormalUser"]) { ?>
+<section>
+	<h2>Subscriptions</h2>
+	<p><a href="view.php"><?= $values["newPostsCount"] ?> new posts</a></p>
+</section>
+	<?php } ?>
 <?php } ?>
 </body>
 </html>
