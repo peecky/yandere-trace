20b70023c75c16c3b17a6bde34b78c1f80daba34
peecky <crimsonpi@gmail.com>
2013-12-11 02:52:57 +0900

    load preview images sequentially

--- www/view.php
+++ www/view.php
@@ -8,7 +8,7 @@ if ($session["isNormalUser"]) {
 	$page = getGet("page", 0);
 	if (!is_numeric($page) || $page < 0) $page = 0;
 
-	$posts = $mysql->query("select p.id postId, p.filename, p.createdDate, p.prefetched from " . TABLE_ACTIVE_ITEM . " ai, " . TABLE_POST . " p
+	$posts = $mysql->query("select p.id postId, p.filename, p.createdDate, p.prefetched, p.memo postMemo from " . TABLE_ACTIVE_ITEM . " ai, " . TABLE_POST . " p
 		where ai.userId = :userId
 		and ai.isRead = FALSE
 		and ai.postId = p.id
@@ -45,11 +45,12 @@ if ($session["isNormalUser"]) {
 	<form method="post" action="action.php">
 	<ul class="thumbnail">
 <?php for ($i = 0, $loops = count($posts); $i < $loops; $i++) { ?>
-		<li><a href="<?= originalPostUrl($posts[$i]) ?>" rel="noreferrer" target="_blank"><img src="<?= thumbmailPath($posts[$i]) ?>" alt="<?= $posts[$i]["postId"] ?>" /></a>
+		<li><span class="thumbnailImage"><img src="<?= thumbmailPath($posts[$i]) ?>" alt="<?= $posts[$i]["postId"] ?>" /></span>
 			<div class="postOptions">
 				<span><label><input type="checkbox" name="read_<?= $posts[$i]["postId"] ?>" checked="checked" />read</label></span>
 			</div>
 			<div class="postInfo">
+				<a href="<?= originalPostUrl($posts[$i]) ?>" rel="noreferrer" target="_blank">See Original</a>
 				<span class="date"><?= $posts[$i]["createdDate"] ?></span>
 			</div>
 		</li>
@@ -61,9 +62,58 @@ if ($session["isNormalUser"]) {
 	</form>
 </section>
 
+<section id="samples">
+</section>
+
 <section id="bottom">
 	<a href="<?= htmlspecialchars($values["pagePreview"]) ?>">preview</a>
 	<a href="<?= htmlspecialchars($values["pageNext"]) ?>">next</a>
 </section>
+
+<script>
+(function() {
+var postMemos = {
+<?php for ($i = 0, $loops = count($posts); $i < $loops; $i++) {
+	if ($i > 0) echo ",\n";
+
+	$postMemo = $posts[$i]["postMemo"];
+	if (empty($postMemo)) $postMemo = "null";
+	echo $posts[$i]["postId"] . ": " . $postMemo;
+} ?>
+};
+
+var requestQueue = [];
+var maxRequestConcurrency = 4;
+var processingRequests = 0;
+
+function processRequest() {
+	if (!requestQueue.length) return;
+	if (processingRequests >= maxRequestConcurrency) return;
+
+	processingRequests++;
+	var postId = requestQueue.shift();
+	$('<img alt="' + postId + '" />')
+		.load(function() {
+			processingRequests--;
+			processRequest();
+		})
+		.click(function() {
+			$(this).remove();
+		})
+		.attr("src", 'request_bridge.php?url=' + postMemos[postId].sample_url)
+		.appendTo($('#samples'));
+}
+
+$('#previews ul.thumbnail .thumbnailImage img').click(function() {
+	var postId = this.alt;
+	if (!postMemos[postId] || !postMemos[postId].sample_url) {
+		alert("sample url not found");
+		return;
+	}
+	requestQueue.push(postId);
+	processRequest();
+});
+})();
+</script>
 </body>
 </html>
