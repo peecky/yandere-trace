394a7c045a57eb0c0e914478ddf94027642e426e
peecky <crimsonpi@gmail.com>
2016-05-25 23:20:21 +0900

    Update request bridge to pipeline remote contents

--- www/request_bridge.php
+++ www/request_bridge.php
@@ -7,5 +7,8 @@ if (!$session["isNormalUser"]) exit();
 
 header('Cache-Control: max-age=31556926');
 header("Content-type: image/jpeg");
-echo file_get_contents($url);
+$context = stream_context_create();
+$fp = fopen($url, 'r', false, $context);
+fpassthru($fp);
+fclose($fp);
 ?>
