d2b936f94a3f98c8a5b186c9c207f690ff8e1dd4
peecky <crimsonpi@gmail.com>
2016-05-25 23:05:26 +0900

    Update request bridge to prevent accessing server side files

--- www/request_bridge.php
+++ www/request_bridge.php
@@ -1,5 +1,7 @@
 <?php
+$url = $_GET["url"];
+if (!filter_var($url, FILTER_VALIDATE_URL)) exit();
 header('Cache-Control: max-age=31556926');
 header("Content-type: image/jpeg");
-echo file_get_contents($_GET["url"]);
+echo file_get_contents($url);
 ?>
