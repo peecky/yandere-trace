8883e894c52083ea7c615be08aaa7212ca088e9e
peecky <crimsonpi@gmail.com>
2016-05-25 23:11:46 +0900

    Update request bridge to allow only logged in users

--- www/request_bridge.php
+++ www/request_bridge.php
@@ -1,6 +1,10 @@
 <?php
 $url = $_GET["url"];
 if (!filter_var($url, FILTER_VALIDATE_URL)) exit();
+
+require_once("common.php");
+if (!$session["isNormalUser"]) exit();
+
 header('Cache-Control: max-age=31556926');
 header("Content-type: image/jpeg");
 echo file_get_contents($url);
