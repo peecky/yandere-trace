c3b6c08b0f3c743e6db691ed3ff9748081010f83
peecky <crimsonpi@gmail.com>
2020-10-19 23:04:22 +0900

    Dependency Bump

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Yandere = void 0;
 const path = require("path");
 const url = require("url");
 const xml2js = require("xml2js");
